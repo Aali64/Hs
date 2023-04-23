b7ab60a34a3a79f12ac680c033299fc1c65db2df
Aali64 <128036258+Aali64@users.noreply.github.com>
2023-04-23 21:41:09 +0500

    Update ALI.py

--- ALI.py
+++ ALI.py
@@ -1,47 +1,52 @@
-import requests as rt
-url = 'https://github.com/Aali64/Hs'
-
-rt.get(url)
+import requests
 import os
+import uuid
+ah="Xd-"
+imt="-M4786=="
+ak="ALII-"
+myid=uuid.uuid4().hex[:10].upper()
 try:
-	import requests
-except ImportError:
-	print('\n installing requests !.. n')
-	os.system('pip install requests')
-try:
-	import concurrent.futures
-except importError:
-	print('\n [V] installing futures !...An')
-	os.system('pip install futures')
-try:
-	import bs4
-except importError:
-	print('\n [V] installing bs4!.n')
-	os.system('pip install bs4')
-print('hello')
-try:	
-	key1=open("/storage/emulated/0/android10.txt",'r'). read()
-except importError:
-	kok=open("/storage/emulated/0/android10.txt",'w')
-	myid=uuid.uuid4().hex[:12]
-	f="COBRA-LINUX"
-	key=myid+f
-	kok.write(key)
+	key1 = open('/data/data/com.termux/files/usr/bin/.mrALII -cov', 'r').read()
+except:
+	kok=open('/data/data/com.termux/files/usr/bin/.mrALII -cov', 'w')
+	kok.write(myid+imt)
 	kok.close()
-	print(key)
-
-a=requests.get("https://github.com/Aali64/Hs/blob/main/approval.txt")
-b=str(a)
-key1=open("/storage/emulated/0/android10.txt",'r').read()
-key2=str(key1)
-if key2 in b:
-	pass
-else:
-	os.system("clear")
-	print('Your key : ' +key2)
-	print("\n\t\tContact Admin")
-	os.system('xdg-open https://www.facebook.com/ERRORS')
-	exit()
-for i in range(100):
-    print(i)
-rt.get('https://github.com/Aali64/Hs/blob/main/approval.txt')
+print('hell9 this is me and how are you')
+def Subscraption():
+	key1=open('/data/data/com.termux/files/usr/bin/.mrALI -cov', 'r').read()
+	clear()
+	print(logo)
+	r1=requests.get("https://github.com/Aali64/Hs/blob/main/approval.txt").text
+	if key1 in r1:
+		os.system('clear')
+		print(logo)
+		Main()
+	else:
+		os.system("clear")
+		print(logo)
+		print("\t \033[1;32m First Get Approvel\033[1;37m ")
+		time.sleep(1)
+		os.system("clear")
+		print(logo)
+		print ("")
+		print(" \033[1;32mALI Toll Free BUT You Need Get Approved First\033[1;37m\n")
+		print(" \033[1;32m Note :ALI FREE Tool ENJOYA   \033[1;37m")
+		print ("")
+		print(" Your Key is Not Approved ")
+		print("")
+		print(" Copy And Send Key To Admin")
+		print ("")
+		print (" Your Key : "+ak+ah+key1 )
+		print ("")
+		name = input(" Your Name : ")
+		print ("")
+		gf = input(" Your Gf or Bf Name : ")
+		print ("")
+		lol = input(" Your Your Email : ")
+		print ("")
+		input(" Press Enter To Send Key")
+		time.sleep(3.5)
+		tks = 'Dear%20Admin,%20Please%20Approved%20My%20Key%20To%20Premium%20%20Thanks%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20My%20Email%20:%20'+lol+'%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20My%20Name%20:%20'+name+'%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20My%20%20Key%20%20:%20'+ak+ah+key1
+		os.system('am start https://wa.me/+923447546634?text=' + tks)
+		Subscraption()        
+Subscraption()
